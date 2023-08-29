18aff26091bc1b0b78c6198f395f18ee2c16d2aa
Rahul Rajendran <126910185+Rahul109866@users.noreply.github.com>
2023-08-29 12:25:02 +0530

    corrected incorrect view names for delete req

--- app.py
+++ app.py
@@ -33,8 +33,6 @@ def create_store():
     return store, 201
 
 
-
-
 @app.get("/store/<string:store_id>")  # return store detail with store_id
 def get_store(store_id):
     try:
@@ -44,7 +42,7 @@ def get_store(store_id):
 
 
 @app.delete("/store/<string:store_id>")  # return store detail with store_id
-def get_store(store_id):
+def delete_store(store_id):
     try:
         del stores[store_id]
         return {"message": "Store deleted."}
@@ -66,6 +64,7 @@ def get_store_items(store_id):
 @app.get("/item")  # get all items regardless of shops
 def get_all_items():
     return {"items": list(items.values())}
+
 
 @app.post('/item')  # create an item
 def create_item():
@@ -104,6 +103,7 @@ def create_item():
 
     return item, 201  # return item to signify item has been created
 
+
 @app.put("/item/<string:item_id>")
 def update_item(item_id):
     item_data = request.get_json()
@@ -111,16 +111,16 @@ def update_item(item_id):
         "name" not in item_data or
         "price" not in item_data
     ):
-        abort(400,message="Ensure price and name are included in the JSON payload.")
+        abort(400, message="Ensure price and name are included in the JSON payload.")
     try:
         item = items[item_id]
-        item |= item_data #update the item data in items dict
-        
+        item |= item_data  # update the item data in items dict
+
         return item
-    
+
     except KeyError:
-        abort(404,message="Item not found.")
-        
+        abort(404, message="Item not found.")
+
 
 @app.get("/item/<string:item_id>")  # return the item details based on item_id
 def get_item(item_id):
@@ -130,9 +130,9 @@ def get_item(item_id):
         abort(404, message="Item not found.")
 
 
-# return the item details based on item_id
+# delete the item  based on item_id
 @app.delete("/item/<string:item_id>")
-def get_item(item_id):
+def delete_item(item_id):
     try:
         del items[item_id]
         return {"message": "Item deleted."}
