bacc6692452cfe5d9420eaba0048e3f6f8d06b77
Rahul Rajendran <126910185+Rahul109866@users.noreply.github.com>
2023-08-29 11:50:28 +0530

    Added endpoints for delete and items

--- app.py
+++ app.py
@@ -17,6 +17,13 @@ def get_data():
 def create_store():
     # use request.get_json() to parse json client payload to python objects
     store_data = request.get_json()
+    if "name" not in store_data:
+        abort(400, message="Ensure Name is included in JSON payload.")
+
+    for store in stores.values():  # check for duplicate store names
+        if store_data["name"] == store["name"]:
+            abort(400, message="Store already exists")
+
     # create a hexadecimal unique user id from uuid module using uuid4 methodology
     store_id = uuid.uuid4().hex
 
@@ -29,6 +36,26 @@ def create_store():
 @app.post('/item')  # create an item
 def create_item():
     item_data = request.get_json()  # parse incoming json item payload to python objects
+    if (
+        "price" not in item_data or
+        "store_id" not in item_data or
+        "name" not in item_data
+
+    ):
+        abort(
+            400,
+            message="Ensure price, name or the store Id are included in the JSON payload"
+        )
+
+    for item in items.values():
+        if (
+            item_data["name"] == item["name"]
+            and item_data["store_id"] == item["store_id"]
+        ):
+            abort(
+                400,
+                message=f"Item already exists."
+            )
 
     # check if the store actually exists to an item to
     if item_data["store_id"] not in stores:
@@ -37,7 +64,7 @@ def create_item():
 
     item_id = uuid.uuid4().hex  # create uuid for item using uuid library
     # same unpacking the payload and attach it with item id
-    item = {**item_data, "id": "item_id"}
+    item = {**item_data, "id": item_id}
     # attach it to item database(dict from db.py) which has item details and store_id
     items[item_id] = item
 
@@ -50,6 +77,25 @@ def get_store(store_id):
         return stores[store_id]
     except KeyError:
         abort(404, message="Store not Found.")
+
+
+@app.delete("/store/<string:store_id>")  # return store detail with store_id
+def get_store(store_id):
+    try:
+        del stores[store_id]
+    except KeyError:
+        abort(404, message="Store not Found.")
+
+
+@app.get("/store/<string:store_id>/items")
+def get_store_items(store_id):
+    try:
+        store_items = [item for item in items.values(
+        ) if item["store_id"] == store_id]
+
+        return {"Store": stores[store_id]["name"], "Items": store_items}
+    except KeyError:
+        return abort(400, message="Store is not found.")
 
 
 @app.get("/item")  # get all items regardless of shops
@@ -65,5 +111,24 @@ def get_item(item_id):
         abort(404, message="Item not found.")
 
 
+# return the item details based on item_id
+@app.delete("/item/<string:item_id>")
+def get_item(item_id):
+    try:
+        del items[item_id]
+    except KeyError:
+        abort(404, message="Item not found.")
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+    print("Stores dictionary contents: ")
+    for store_id, store in stores.items():
+        print(f"Store ID: {store_id},\nStore Name: {store}")
+
+    print("Items dictionary contents: ")
+    for item_id, item in items.items():
+        print(f"Item ID: {item_id}")
+        for key, value in item.items():
+            print(f"  {key}: {value}")
